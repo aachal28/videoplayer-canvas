bce7448d850e250d1e62bb4127fa40f65a168593
aachal28 <achalpardeshi159@gmail.com>
2023-11-10 15:04:42 +0530

    added css to videoInput

--- src/Components/VideoInput.js
+++ src/Components/VideoInput.js
@@ -1,10 +1,31 @@
 // VideoInput.js
-import React from "react";
+import React, { useState } from "react";
 
 const VideoInput = ({ handleFileChange }) => {
+  const [selectedFileName, setSelectedFileName] = useState(null);
+
+  const handleFileSelection = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFileName("Select Video");
+      handleFileChange(e);
+    }
+  };
+
   return (
     <form>
-      <input type="file" onChange={handleFileChange} />
+      <label
+        htmlFor="fileInput"
+        className="fileInputLabel"
+      >
+        {selectedFileName || "Select Video"}
+      </label>
+      <input
+        id="fileInput"
+        type="file"
+        onChange={handleFileSelection}
+        style={{ display: "none" }}
+      />
     </form>
   );
 };
